80b76aa8c5e107af6e701c0973ea0be1467eed24
Zach B. Harris <zachbharris94@gmail.com>
2018-12-15 16:19:40 -0600

    basic theme yo

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,25 @@ import theme from 'styled-theming'
 // import router for pages
 import Router from './router';
 
+const backgroundColor = theme("mode", {
+  light: "#F4F5F1",
+  dark: "#0B0B0A"
+})
+
 const GlobalStyles = createGlobalStyle`
   body {
     margin: 0;
+    background: ${backgroundColor}
   }
 `
 
 class App extends Component {
+  state = {
+    mode: "light"
+  }
   render() {
     return (
-      <ThemeProvider theme={{ mode: 'default' }}>
+      <ThemeProvider theme={{ mode: this.state.mode }}>
         <Fragment>
           <Router />
           <GlobalStyles />
