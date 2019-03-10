f44e39d3538f2bb065ba0c7a259a7f19cec20d4c
zachbharris <zachbharris94@gmail.com>
2019-03-10 17:54:24 -0500

    fml

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -4,8 +4,7 @@ const preferDefault = m => m && m.default || m
 exports.components = {
   "component---cache-dev-404-page-js": () => import("/Users/Zach/git/sirrah/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-404-js": () => import("/Users/Zach/git/sirrah/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
-  "component---src-pages-index-js": () => import("/Users/Zach/git/sirrah/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
-  "component---src-pages-page-2-js": () => import("/Users/Zach/git/sirrah/src/pages/page-2.js" /* webpackChunkName: "component---src-pages-page-2-js" */)
+  "component---src-pages-index-js": () => import("/Users/Zach/git/sirrah/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */)
 }
 
 exports.data = () => import(/* webpackChunkName: "pages-manifest" */ "/Users/Zach/git/sirrah/.cache/data.json")
