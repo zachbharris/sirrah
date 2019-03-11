e42a5cd323f35f93f83a504743de10ed6c657cc7
zachbharris <zachbharris94@gmail.com>
2019-03-10 19:25:58 -0500

    i dont know

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { createGlobalStyle } from "styled-components";
 
 import Routes from "./routes";
+
+const GlobalStyles = createGlobalStyle`
+  html, body {
+    font-family: 'Open Sans', Helvetica, Arial, sans-serif;
+    font-size: 16px;
+    margin: 0;
+    padding: 0;
+  }
+`;
 
 const App = () => {
   return (
     <>
+      <GlobalStyles />
       <Routes />
     </>
   )
