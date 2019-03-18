d996c2ad091c95a10019e27da0f7c796cadc82b0
Zachary Harris <zachbharris94@gmail.com>
2019-03-17 21:54:24 -0500

    theme stuff and misc

---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Layout = ({ container }) => {};
+
+Layout.propTypes = {
+  container: PropTypes.bool
+};
+
+Layout.defaultProps = {
+  container: false
+};
+
+export default Layout;
---
+++ src/theme/colors.js
@@ -0,0 +1,2 @@
+export const white = "#FFFFFF";
+export const black = "#1B1C1D";
